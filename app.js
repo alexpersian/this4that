4c590c78e73c19f5ab8abbfcf66638a38a56977a
Steve Henningsgard <hest0401@stcloudstate.edu>
2015-03-16 18:51:57 -0500

    Dickhole

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var http = require("http");
 
 var server = http.createServer(function (req, res) {
   res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end("FUCK YESH");
+  res.end("FUCK YESH!!");
 
 });
 
